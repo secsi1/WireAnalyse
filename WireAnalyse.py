175cd21509598b338c8e593feba1e11eb3bdc25e
九思 <yeluo945@163.com>
2023-06-03 23:32:01 +0800

    WireShark分析脚本第一版

--- WireAnalyse.py
+++ WireAnalyse.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+#author:九思
+#github:https://github.com/secsi1/WireAnalyse
+
 import pyshark
 import hashlib
 import time
+print(" ")
+print(" / / /\ \ (_)_ __ ___  /_\  _ __   __ _| |_   _ ___  ___  ")
+print(" \ \/  \/ / | '__/ _ \//_\\| '_ \ / _` | | | | / __|/ _ \ ")
+print("  \  /\  /| | | |  __/  _  \ | | | (_| | | |_| \__ \  __/ ")
+print("   \/  \/ |_|_|  \___\_/ \_/_| |_|\__,_|_|\__, |___/\___| ")
+print("                                         |___/            ")
 
 def analyze_traffic(pcap_file):
     capture = pyshark.FileCapture(pcap_file)
